3026606cda38d10bc9bfba073eb000398f028671
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-06-01 06:08:05 -0600

    Add properties to returned result

--- src/utilities.js
+++ src/utilities.js
@@ -1,16 +1,24 @@
 export const cleanResponse = (response) => {
+  console.log(response)
   const result = {};
   result.section = response.section;
   result.num_results = response.num_results;
+  result.copyright = response.copyright;
   result.articles = response.results.map((article) => {
     return {
-      section: article.secton,
+      section: article.section,
+      byline: article.byline,
       subsection: article.subsection,
       abstract: article.abstract,
       image: article.multimedia[0],
       published_date: article.published_date,
       url: article.url,
-      title: article.title, 
+      title: article.title,
+      location: article.geo_facet,
+      people: article.per_facet,
+      organization: article.org_facet
     }
   })
+
+  return result;
 }
