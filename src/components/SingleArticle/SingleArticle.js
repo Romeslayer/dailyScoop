1aa3d728c108498df986abe0c2eab32b4fe1d537
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-06-01 16:30:53 -0600

    Add more content to singe article display

--- src/components/SingleArticle/SingleArticle.js
+++ src/components/SingleArticle/SingleArticle.js
@@ -10,16 +10,21 @@ function SingleArticle ({article}) {
 
   return (
     <div className="single-article">
+      <div className="content">
       <h4 className="article-section-name">{article.section}</h4>
       <h2 className="article-title">{article.title}</h2>
       <h3 className="article-byline">{article.byline}</h3>
-      <button onClick={() => {
+      <button className="read-more" onClick={() => {
         window.open(article.url)
       }}> Read More</button>
+      </div>
       <img className="single-img" src={article.image[0].url} alt={article.image[0].caption} />
       <p className="article-abstract black">{article.abstract}</p>
       {article.subsection && <p>Subsection: {article.subsection}</p>}
       {article.published_date && <p>Published: {`${publishedDate.getMonth() + 1}/${publishedDate.getDate()}/${publishedDate.getFullYear()} `}</p>}
+      {article.location.length >= 1 ? <p>Locations invloved: {article.location.join(', ')}</p> : '' }
+      {article.people.length >= 1 ? <p>People invloved: {article.people.join(', ')}</p> : ''} 
+      {article.organization >= 1 ? <p>Organization: {article.organization.join(', ')}</p> : ''}
     </div>
   )
 }
