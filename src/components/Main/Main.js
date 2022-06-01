41d7ab3ecda48b6db133806ad64a3c2c70d8594f
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-06-01 08:43:18 -0600

    Create Main component that renders article titles

---
+++ src/components/Main/Main.js
@@ -0,0 +1,26 @@
+import './Main.css'
+import React from 'react'
+
+function Main({ articles, section }) {
+
+  const buildArticles = () => {
+    if(articles.length) {
+     return articles.map((article) => <p key={article.title + article.abstract}>{article.title}</p>)
+    }
+  }
+  const buildTitle = () => {
+    if(!section) return <h2>Today's Top Stories:</h2>
+    return (
+      <h2>Today's {section} Stories:</h2>
+    )
+  }
+
+  return (
+    <main>
+      {buildTitle()}
+      {buildArticles()}
+    </main>
+  )
+}
+
+export default Main
